26b5e96a22efe5959bb151d21499b0889efc76de
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-02-08 14:04:25 +0000

    Updated meal_item and meal_details widgets to include Hero animations. In both the widgets, FadeInImage and Image are now wrapped in a Hero widget, which provides a smooth transition between images. Also, in the meal_details widget, the RotationTransition was improved by adding a Tween animation, providing better visual feedback when the star icon is toggled.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -38,11 +38,17 @@ class MealDetailsScreen extends ConsumerWidget {
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                  turns: animation,
+                  turns: Tween<double>(
+                    begin: 0.8,
+                    end: 1,
+                  ).animate(animation),
                   child: child,
                 );
               },
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           )
         ],
@@ -50,11 +56,14 @@ class MealDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
           child: Column(
         children: [
-          Image.network(
-            meal.imageUrl,
-            height: 300,
-            width: double.infinity,
-            fit: BoxFit.cover,
+          Hero(
+            tag: meal.id,
+            child: Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
           const SizedBox(
             height: 14,
